bc55bfc52989a357eb851006bcbc6238d0c9daaa
Memunat Yusuf <memunat243@gmail.com>
2025-03-04 13:47:43 -0800

    add offensive word count

--- Js/script.js
+++ Js/script.js
@@ -14,14 +14,14 @@ function offensiveWord() {
     return bad
 }
 
-// function noInputtedWord() {
-//     for (let i = 0; i < arguments.length; i++) {
-//         if (arguments[i].trim().length === 0) {
-//             return true;
-//         }
-//     }
-//     return false;
-// }
+function noInputtedWord() {
+    for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i].trim().length === 0) {
+            return true;
+        }
+    }
+    return false;
+}
 
 //BUSINESS-LOGIC//
 
